a9df5d3aa7258460f367e9262de02b019f7a550d
cappashop <152763963+cappashop@users.noreply.github.com>
2024-01-21 17:31:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from ultralytics import YOLO
 
 # Replace the relative path to your weight file
-model_path = 'weights/best.pt'
+model_path = 'weights/best(1).pt'
 
 # Setting page layout
 st.set_page_config(
